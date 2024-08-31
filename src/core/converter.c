a9d5393d700a847ae278eca3090dd829e8f6aea7
DragonSlayer <varattleff@Viktors-MacBook-Air.local>
2024-08-31 22:19:07 +0200

    seperation of concers refactor, and return to main menu functionality added

--- src/core/converter.c
+++ src/core/converter.c
@@ -2,12 +2,19 @@
 #include "valuta_converter.h"
 #include "colors.h"
 
-int main () 
+void handle_currency_selection();
+void handle_conversion_target();
+
+int main() 
 {
     display_intro();
+    handle_currency_selection();
+    
+    return 0;
+}
 
-    //step one 
-
+void handle_currency_selection()
+{
     int valid_input_selected_currency = 0;
     int selected_currency;
 
@@ -29,24 +36,28 @@ int main ()
             valid_input_selected_currency = 1;
         }
     }
-    
+
     switch (selected_currency) 
     {
         case 1:
             printf("\n" "You selected option 1 " BLUE "USD " RESET_COLOR);
             display_conversion_target_options(); 
+            handle_conversion_target();
             break;
         case 2:
             printf("\n" "You selected option 2 " YELLOW "EUR " RESET_COLOR);
             display_conversion_target_options(); 
+            handle_conversion_target();
             break;
         case 3:
-            printf("\n" "You selected option 3 " RED "DKK " RESET_COLOR);
+            display_conversion_target_options(); 
+            handle_conversion_target();
             break;
     }
+}
 
-    //step two 
-
+void handle_conversion_target()
+{
     int valid_input_selected_target_currency = 0;
     int selected_target_currency;
 
@@ -79,11 +90,7 @@ int main ()
             printf("\n" "You selected option 3 " RED "DKK " RESET_COLOR);
             break;
         case 4:
-            printf("\n" "You selected option 4 " GREEN "NOK " RESET_COLOR); 
+            handle_currency_selection();
             break;
     }
-
-    //step three
-
-    return 0;
 }
