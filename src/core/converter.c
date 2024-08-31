1ffebacf922c1ba3e5d193e88915950f273618f9
DragonSlayer <varattleff@Viktors-MacBook-Air.local>
2024-08-31 21:10:34 +0200

    added logic and errorhandeling on input in display_currency_options()

--- src/core/converter.c
+++ src/core/converter.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
 #include "valuta_converter.h"
+#include "colors.h"
 
 int main () 
 {
     display_intro();
-    display_currency_options();
-    display_conversion_target_options(); 
+
+    int valid_input = 0;
+    int selected_currency;
+
+    while (!valid_input)
+    {
+        display_currency_options();
+
+        if (scanf("%d", &selected_currency) != 1) 
+        {
+            while (getchar() != '\n'); 
+            printf("\n" RED "Invalid choice. Please enter a number between 1 and 3.\n" RESET_COLOR);
+        } 
+        else if (selected_currency < 1 || selected_currency > 3) 
+        {
+            printf("\n" RED "Invalid choice. Please enter a number between 1 and 3.\n" RESET_COLOR);
+        } 
+        else 
+        {
+            valid_input = 1;
+        }
+    }
+    
+   switch (selected_currency) 
+    {
+        case 1:
+            printf("You selected option 1.\n");
+            break;
+        case 2:
+            printf("You selected option 2.\n");
+            break;
+        case 3:
+            printf("You selected option 3.\n");
+            break;
+    }
+    
     return 0;
 }
--- src/gui/valuta_converter_gui.c
+++ src/gui/valuta_converter_gui.c
@@ -15,11 +15,6 @@ void display_intro()
     printf("Here you can easily convert between the world's most commonly used currencies:\n\n");
 
     print_all_flags();
-    printf("\n");
-
-    printf("--> " BLUE "USD" RESET_COLOR " (US Dollar / Freedom Unit)\n");
-    printf("--> " YELLOW "EUR" RESET_COLOR " (Euro)\n");
-    printf("--> " RED "DKK" RESET_COLOR " (Danish Krone)\n");
 }
 
 void display_currency_options() 
@@ -27,9 +22,12 @@ void display_currency_options()
     printf("\n");
     printf("Please select the unit you want to convert by entering the corresponding number and pressing Enter:\n\n");
 
-    printf(" (1) " BLUE "USD" RESET_COLOR "\n");
-    printf(" (2) " YELLOW "EUR" RESET_COLOR "\n");
-    printf(" (3) " RED "DKK" RESET_COLOR "\n");
+    printf(" (1) " BLUE "USD" RESET_COLOR  " (US Dollar / Freedom Unit)" "\n");
+    printf(" (2) " YELLOW "EUR" RESET_COLOR " Euro" "\n");
+    printf(" (3) " RED "DKK" RESET_COLOR " Danish Krone" "\n");
+
+    printf("\n");
+    printf("Enter your choice (1-3): ");
 }
 
 void display_conversion_target_options() 
@@ -40,4 +38,7 @@ void display_conversion_target_options()
     printf(" (1) " BLUE "USD" RESET_COLOR " (US Dollar)\n");
     printf(" (2) " YELLOW "EUR" RESET_COLOR " (Euro)\n");
     printf(" (3) " RED "DKK" RESET_COLOR " (Danish Krone)\n");
+
+    printf("\n");
+    printf("Enter your choice (1-3): ");
 }
